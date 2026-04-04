5e13208a726779a4c79f5efc3b82dbe705fbeadb
KyleDunbarDev <kyledunbar9000@gmail.com>
2026-04-04 19:46:16 +0100

    add currency

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,39 @@
 use std::collections::HashMap;
 
+#[derive(Hash, PartialEq, Eq)]
 enum Currency {
-    coins,
-    diamonds,
+    Coins,
+    Diamonds,
 }
 impl Currency {}
 
+#[derive(Default)]
 struct Wallet {
     balances: HashMap<Currency, i64>,
 }
-impl Wallet {}
+impl Wallet {
+    fn add_currency(&mut self, curr: Currency, amount: i64) {
+        *self.balances.entry(curr).or_insert(0) += amount;
+    }
+}
 
 fn main() {
-    println!("Hello, world!");
+    println!("Hello world!");
+}
+
+// ================ TESTS =====================
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_gain_coins() {
+        let mut wallet = Wallet::default();
+        wallet.add_currency(Currency::Coins, 0);
+        assert_eq!(wallet.balances.get(&Currency::Coins), Some(&0));
+        wallet.add_currency(Currency::Coins, 4);
+        assert_eq!(wallet.balances.get(&Currency::Coins), Some(&4));
+        wallet.add_currency(Currency::Coins, 3);
+        assert_eq!(wallet.balances.get(&Currency::Coins), Some(&7));
+    }
 }
